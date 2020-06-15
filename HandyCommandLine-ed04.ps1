12a0de78a4222e31b4fb4e1e46603334a5c1d160
retrospectivePreposterous <61591245+retrospectivePreposterous@users.noreply.github.com>
2020-06-15 21:55:41 +0200

    Add files via upload

---
+++ HandyCommandLine-ed04.ps1
@@ -0,0 +1,18 @@
+﻿$port = New-Object System.IO.Ports.SerialPort
+$port.PortName = "COM7" #RENAME TO YOUR CURRENT PORT.
+$port.BaudRate = "9600"
+$port.open() 
+
+Start-Sleep -Milliseconds 500
+Write-Host " [HandyCommandLine 1.0 -- by retrospectivePreposterous]"
+Write-Host " Enter EXIT to close. Readlines are disabled in this feature"
+Write-Host "----------------------------------------------------------------"
+do {
+$codeline = Read-Host 
+if ($codeline -ne "exit") {
+$port.Write("$codeline`r") 
+Start-Sleep -Milliseconds 500
+}
+} until ( $codeline -eq "exit" ) 
+
+$port.close() 
